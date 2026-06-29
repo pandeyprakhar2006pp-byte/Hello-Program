dcf5424a1ca3f2e480b7f8a728d88a3874a4b4ac
Prakhar Pandey <pandeyprakhar2006pp@gmail.com>
2026-06-29 18:01:53 +0530

    Time: 0 ms (100%), Space: 43.7 MB (96.69%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,27 +1,38 @@
-class Solution {
-    public int findPeakElement(int[] nums) {
+public class Solution {
 
-        int n = nums.length;
+    public static int findPeakElement(int[] arr) {
+        int n = arr.length;
 
-        if (n == 1) {
+        // Only one element
+        if (n == 1)
             return 0;
-        }
 
-        if (nums[0] > nums[1]) {
+        // Peak at first index
+        if (arr[0] > arr[1])
             return 0;
-        }
 
-        if (nums[n - 1] > nums[n - 2]) {
+        // Peak at last index
+        if (arr[n - 1] > arr[n - 2])
             return n - 1;
-        }
 
-        for (int i = 1; i < n - 1; i++) {
+        int low = 1;
+        int high = n - 2;
 
-            if (nums[i] > nums[i - 1] && nums[i] > nums[i + 1]) {
-                return i;
-            }
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            // Peak found
+            if (arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1])
+                return mid;
+
+            // Move towards increasing slope
+            if (arr[mid] > arr[mid - 1])
+                low = mid + 1;
+            else
+                high = mid - 1;
         }
 
         return -1;
     }
+
 }
