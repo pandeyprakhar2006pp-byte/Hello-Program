cc50868ef8df28c52e8d51775b63afc40ce14c20
Prakhar Pandey <pandeyprakhar2006pp@gmail.com>
2026-08-11 07:51:24 +0530

    Time: 14 ms (91.08%), Space: 42.5 MB (68.5%) - LeetHub

---
+++ 1510-stone-game-iv/1510-stone-game-iv.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean winnerSquareGame(int n) {
+        // dp[i] will store true if the first player to move can win with i stones
+        boolean[] dp = new boolean[n + 1];
+        
+        // Base case: dp[0] is false (default in Java booleans)
+        
+        for (int i = 1; i <= n; i++) {
+            // Try all possible square numbers k*k <= i
+            for (int k = 1; k * k <= i; k++) {
+                // If leaving (i - k*k) stones puts the next player in a losing state,
+                // then the current player wins!
+                if (!dp[i - k * k]) {
+                    dp[i] = true;
+                    break; // No need to check further square numbers for this 'i'
+                }
+            }
+        }
+        
+        return dp[n];
+    }
+}
